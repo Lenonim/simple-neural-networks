17a571c74926911a7fe2e5757bb35c8f68df3962
Lenonim <ilja.ist.am.besten@mail.ru>
2023-09-30 23:57:45 +0300

    Улучшил классификатор чисел

--- numbers_classification/src/Neirons.h
+++ numbers_classification/src/Neirons.h
@@ -1,13 +1,13 @@
 #pragma once
 #include "DataSets.h"
 #include <ctime>
-#include <xmemory>
+#include <thread>
 
 class NumsPerceptronPF
 {
 private:
     int n_sensors = 15;
-    int threshold = 9;
+    int threshold = 0;
     int sum = 0;
     int weights[15] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     int* tema_num;
@@ -17,6 +17,7 @@ private:
         for (int i = 0; i < 15; i++)
             if (tema_num[i] == 1)
                 this->weights[i] -= 1;
+        threshold += 1;
     }
     void increase(int num) {
         tema_num = stdnums::nums[num];
@@ -26,21 +27,43 @@ private:
     }
 
 public:
-    void fit(int tema, int n_lessons = 5000) { // n_lessons - epochs
+    void fit(int tema, int n_lessons = 500, bool whis_random = true) { // n_lessons - epochs
         srand(time(NULL));
         this->tema_num = stdnums::nums[tema];
         for (int i = 0; i < n_lessons; i++) {
-            int rand_index = rand() % 10;
+            int index;
+            if (whis_random)
+                index = rand() % 10;
+            else
+                index = i % 10;
 
-            if (rand_index != tema) {
-                if (predict(stdnums::nums[rand_index]))
-                    decrease(rand_index);
+            if (index != tema) {
+                if (predict(stdnums::nums[index]))
+                    decrease(index);
             }
             else {
-                if (!predict(stdnums::nums[rand_index]))
-                    increase(rand_index);
+                if (!predict(stdnums::nums[index]))
+                    increase(index);
             }
         }
+    }
+    bool is_in(int a) {
+        double b = double(a);
+        __int16 temp;
+        if (b / 10 < 0)
+            return this->predict(stdnums::nums[a]);
+        else
+            while (b > 0) {;
+                int fractPart;
+                b /= 10;
+                double* intPart = new double();
+                fractPart = int(10 * modf(b, intPart));
+                b = *intPart;
+                delete intPart;
+                if (this->predict(stdnums::nums[fractPart]))
+                    return true;
+            }
+        return false;
     }
     bool predict(int num[15]) {
         this->sum = 0;
@@ -53,3 +76,34 @@ public:
             return false;
     }
 };
+
+class ClassificationNetwork {
+    NumsPerceptronPF* NumsNeurons;
+    __int16 digits;
+
+public:
+    __int16 predict(int num[15]) {
+        for (__int16 i = 0; i < digits; i++) {
+            if (NumsNeurons[i].predict(num))
+                return i;
+        }
+        return -1;
+    }
+    int sequenc_to_int(int Seq[], int size, int nums[][15] = stdnums::nums) {
+        int Sum = 0;
+        for (int i = 0; i < 4; i++)
+            Sum += this->predict(minnums::nums[Seq[i]]) * pow(10, size - 1 - i);
+        return Sum;
+    }
+
+    ClassificationNetwork() {
+        digits = 10;
+        NumsNeurons = new NumsPerceptronPF[digits];
+        for (__int16 i = 0; i < digits; i++)
+            NumsNeurons[i].fit(i, 1000, true);
+    }
+    ~ClassificationNetwork() {
+        delete[] NumsNeurons;
+    }
+};
+
--- numbers_classification/src/Netz.cpp
+++ numbers_classification/src/Netz.cpp
@@ -3,14 +3,33 @@
 
 int main() {
     srand(time(NULL));
+
+    // простое распознавание ( да или нет ) на поданное число
+    std::cout << "fit on 5 and check on standart data\n";
     NumsPerceptronPF pf;
     pf.fit(5);
     for (int i = 0; i < 10; i++) {
         std::cout << i << " answer of network = " << pf.predict(stdnums::nums[i]) << "\n";
     }
-    std::cout << "\n";
-    pf.fit(9, 10000);
+    std::cout << "\nrefit on 8 and check on other data\n";
+    pf.fit(8, 2000);
     for (int i = 0; i < 10; i++) {
         std::cout << i << " answer of network = " << pf.predict(minnums::nums[i]) << "\n";
     }
+
+    std::cout << "\ncheck numbers on tema number\n";
+    std::cout << "Is in 546 fit number? answer = " << pf.is_in(546) << '\n';
+    std::cout << "Is in 873 fit number? answer = " << pf.is_in(873) << '\n';
+
+    // комплексное распознование ( это есть ____ цифра) на поданное число
+    std::cout << "\ncheck answers of classification network\n";
+    ClassificationNetwork classnet;
+    for (int i = 0; i < 10; i++) {
+        std::cout << "put " << i << " number to network; answer of network: is it " << classnet.predict(minnums::nums[i]) << "\n";
+    }
+
+    // создадим последовательность из массивов
+    const int SeqSize = 4;
+    int SeqNums[SeqSize] = { 1, 4, 9, 0 };
+    std::cout << "\nnumber sequens (1490) to int; answer = " << classnet.sequenc_to_int(SeqNums, SeqSize);
 }
